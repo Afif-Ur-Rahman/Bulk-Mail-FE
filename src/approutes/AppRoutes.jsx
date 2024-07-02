fcb05508507fe1d628b5df0f31f3087596960730
Afif-Ur-Rahman <afifurrahman444@gmail.com>
2024-07-02 15:29:48 +0500

    removed route authentication for now

--- src/approutes/AppRoutes.jsx
+++ src/approutes/AppRoutes.jsx
@@ -1,32 +1,36 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+// import { useEffect, useState } from "react";
+import {
+  Routes,
+  Route,
+  // useNavigate
+} from "react-router-dom";
 import { Signup, Login } from "../components";
 import Home from "../pages/Home/Home";
 import EmailTemplate from "../pages/Email/EmailTemplate";
 
 const AppRoutes = () => {
-  const navigate = useNavigate();
-  const token = localStorage.getItem("token");
-  const [isToken, setIsToken] = useState(!!token);
+  // const navigate = useNavigate();
+  // const token = localStorage.getItem("token");
+  // const [isToken, setIsToken] = useState(!!token);
 
-  useEffect(() => {
-    setIsToken(!!token);
-  }, [token]);
+  // useEffect(() => {
+  //   setIsToken(!!token);
+  // }, [token]);
 
-  useEffect(() => {
-    if (!isToken) {
-      navigate("/");
-    } else {
-      navigate("/home");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (!isToken) {
+  //     navigate("/");
+  //   } else {
+  //     navigate("/home");
+  //   }
+  // }, []);
   return (
     <>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Login />} />
-        {isToken && <Route path="/home" element={<Home />} />}
+        <Route path="/home" element={<Home />} />
         <Route path="/emailtemplates" element={<EmailTemplate />} />
       </Routes>
     </>
